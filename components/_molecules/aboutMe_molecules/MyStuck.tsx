9c49e22052af923e46d093acf374c731c11478bc
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2025-04-22 00:16:12 +0400

    fix/margin

--- components/_molecules/aboutMe_molecules/MyStuck.tsx
+++ components/_molecules/aboutMe_molecules/MyStuck.tsx
@@ -8,7 +8,7 @@ import ToolsStuck from '@/components/_atoms/aboutMe_atoms/ToolsStuck';
 const MyStuck = () => {
   return (
     <div className='mt-[100px] w-full'>
-      <div className='px-[50px] py-[50px] flex justify-center'>
+      <div className='px-[50px] py-[50px] mb-[50px] flex justify-center'>
         <h2 className='firaCode font-bold text-[90px] leading-[90px] text-white drop-shadow-2xl '>
           My Stuck
         </h2>
