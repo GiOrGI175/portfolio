f7dc551406592f2ef5e8732278715940e16618e4
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2025-04-24 00:50:40 +0400

    fix/section

--- components/_organisms/githubContributions _organisms/GithubSection.tsx
+++ components/_organisms/githubContributions _organisms/GithubSection.tsx
@@ -4,7 +4,12 @@ import GithubContributions from './GithubContributions ';
 const GithubSection = () => {
   return (
     <div className='hero_gradient w-full flex justify-center'>
-      <div className='max-w-[1440px] w-full flex flex-col items-center'>
+      <div className='max-w-[1440px] w-full flex flex-col items-center py-[120px]'>
+        <div className=' mb-[100px]'>
+          <h3 className='firaCode font-bold text-[90px] leading-[90px] text-white drop-shadow-2xl '>
+            GitHub contributions
+          </h3>
+        </div>
         <GithubContributions />
       </div>
     </div>
