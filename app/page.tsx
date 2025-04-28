5861c6c9f8f13afae70eb64cbb9e0b24f7d1d9e8
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2025-04-28 22:27:56 +0400

    feature/contact section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import GithubSection from '@/components/_organisms/githubContributions _organisms/GithubSection';
 import HomePage from './(view)/home/page';
 import AboutSection from '@/components/_organisms/aboutMe_organisms/AboutSection';
+import ContactMe from '@/components/_organisms/contactMe_organisms/ContactMe';
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <HomePage />
       <AboutSection />
       <GithubSection />
+      <ContactMe />
     </>
   );
 }
---
+++ components/_molecules/contactMe_moloecules/ContactMeForm.tsx
@@ -0,0 +1,43 @@
+'use client';
+const ContactMeForm = () => {
+  return (
+    <div className='relative flex max-w-[500px] min-h-[550px] w-full border p-[20px] border-[#9911ff] rounded-4xl z-20'>
+      <form className='flex flex-col gap-[40px] w-full h-full'>
+        <label className='flex flex-col '>
+          <span className='firaCode m-[10px]  text-[white]'>full name *</span>
+          <input
+            type='text'
+            placeholder='Enter your full name'
+            className='firaCode border w-full h-[40px] rounded-4xl p-[20px] border-[#9911ff]  text-[white]'
+          />
+        </label>
+        <label className='flex flex-col'>
+          <span className='firaCode m-[10px]  text-[white]'> Email *</span>
+          <input
+            type='text'
+            placeholder='Enter your email'
+            className='firaCode border w-full h-[40px] rounded-4xl p-[20px] border-[#9911ff]  text-[white]'
+          />
+        </label>
+        <label className='flex flex-col'>
+          <span className='firaCode m-[10px]  text-[white]'>Message *</span>
+          <textarea
+            placeholder='Enter your Message'
+            className='firaCode border w-full h-[40px] rounded-4xl p-[20px] min-h-[200px]  border-[#9911ff]  text-[white]'
+          />
+        </label>
+        <button
+          type='submit'
+          className='w-[100px] h-[40px] rounded-4xl bg-[#9911ff] flex justify-center items-center  self-center cursor-pointer'
+        >
+          <span className='firaCode m-[10px]  text-[white] text-center'>
+            submit
+          </span>
+        </button>
+      </form>
+      <div className='absolute bottom-[-200px] left-[-300px] z-10 w-[500px] h-[500px] rounded-full bg-[#340a83] blur-[300px]' />
+    </div>
+  );
+};
+
+export default ContactMeForm;
