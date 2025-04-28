48fc15e4d2ac0ae9951f671efb6c295fabb9995a
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2025-04-28 19:06:24 +0400

    feature/constact me section

--- components/_organisms/contactMe_organisms/ContactMe.tsx
+++ components/_organisms/contactMe_organisms/ContactMe.tsx
@@ -1,6 +1,10 @@
 'use client';
 const ContactMe = () => {
-  return <div></div>;
+  return (
+    <div className='hero_gradient w-full flex justify-center'>
+      <div className='max-w-[1440px] w-full flex flex-col items-center'></div>
+    </div>
+  );
 };
 
 export default ContactMe;
