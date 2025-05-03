26abcb58281db5ef07b9a4fb8735ad3c9ccde676
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2025-05-03 20:02:38 +0400

    feature/project section

--- app/(view)/blog/BlogMain.tsx
+++
@@ -1,3 +0,0 @@
-export default function AboutMeMain() {
-  return <div className='hero_gradient w-full flex justify-center'></div>;
-}
--- components/_molecules/aboutMe_molecules/ProjectSection.tsx
+++ components/_molecules/aboutMe_molecules/ProjectSection.tsx
@@ -1,8 +1,7 @@
 'use client';
 
-import { projects } from '@/commons/services/projects';
+import ProjectsArry from '@/components/_atoms/aboutMe_atoms/ProjectsArry';
 import { motion, useInView } from 'framer-motion';
-import Image from 'next/image';
 import Link from 'next/link';
 import { useRef } from 'react';
 
@@ -30,96 +29,17 @@ const ProjectSection = () => {
         </motion.div>
 
         <div className='w-full'>
-          {projects.slice(0, 3).map((item, index) => {
-            const ElementRef = useRef(null);
-            const isInViewEl = useInView(ElementRef, {
-              once: false,
-              margin: '-100px',
-            });
-
-            return (
-              <div
-                key={item.link}
-                className='flex flex-col overflow-hidden'
-                ref={ElementRef}
-              >
-                <div
-                  className={`flex ${
-                    (index + 1) % 2 === 0 ? 'flex-row-reverse' : 'flex-row'
-                  } items-center justify-between   ${
-                    index + 1 === 3 ? 'mb-[0px]' : ' mb-[200px]'
-                  }  `}
-                >
-                  <Link
-                    href={item.link}
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    className='flex flex-col items-center'
-                  >
-                    <motion.span
-                      className='firaCode text-[40px] leading-[50px] text-white mb-[30px] ml-[50px]'
-                      initial={{
-                        x: (index + 1) % 2 === 0 ? '-100vw' : '100vw',
-                      }}
-                      animate={isInViewEl ? { x: 0 } : undefined}
-                      transition={{
-                        duration: 0.8,
-                        ease: 'easeOut',
-                        type: 'spring',
-                        stiffness: 120,
-                      }}
-                    >
-                      {item.name} {item.mount}-{item.year}
-                    </motion.span>
-                    <motion.div
-                      className='rounded-2xl overflow-hidden'
-                      initial={{
-                        x: (index + 1) % 2 === 0 ? '100vw' : '-100vw',
-                      }}
-                      animate={isInViewEl ? { x: 0 } : undefined}
-                      transition={{
-                        duration: 0.8,
-                        ease: 'easeOut',
-                        type: 'spring',
-                        stiffness: 120,
-                      }}
-                    >
-                      <Image
-                        src={item.img}
-                        width={700}
-                        height={600}
-                        alt='WEB PAGE'
-                      />
-                    </motion.div>
-                  </Link>
-                  <motion.div
-                    className='max-w-[500px] w-full h-[332px] px-[20px] flex justify-center items-center'
-                    initial={{ y: '100vw' }}
-                    animate={isInViewEl ? { y: 0 } : undefined}
-                    transition={{
-                      duration: 0.8,
-                      ease: 'easeOut',
-                      type: 'spring',
-                      stiffness: 120,
-                    }}
-                  >
-                    <p className='firaCode text-[20px] leading-[50px] text-white '>
-                      {item.info}
-                    </p>
-                  </motion.div>
-                </div>
-              </div>
-            );
-          })}
+          <ProjectsArry />
         </div>
         <div className='w-full mt-[60px] flex justify-end'>
           <motion.div
-            whileHover={{ scale: 1.2 }}
+            whileHover={{
+              scale: [1, 1.2, 1],
+            }}
             transition={{
-              duration: 0.8,
-              ease: 'easeOut',
-              type: 'spring',
-              stiffness: 120,
+              duration: 1.2,
+              repeat: Infinity,
+              ease: 'easeInOut',
             }}
           >
             <Link href='/projects'>
