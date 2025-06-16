7efa4fe992338ba086583ecb48bafefd43c38b4f
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2025-06-17 00:31:43 +0400

    feature/email sender

--- components/_molecules/contactMe_moloecules/ContactMeForm.tsx
+++ components/_molecules/contactMe_moloecules/ContactMeForm.tsx
@@ -4,8 +4,11 @@ import darkModeStore from '@/commons/hooks/darkModeStore';
 import LangTranstionSpan from '@/lib/LangTranstionSpan';
 import { useTranslations } from 'next-intl';
 import { useLocale } from 'next-intl';
+import { useState } from 'react';
 
 const ContactMeForm = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
   const darkMode = darkModeStore((state) => state.darkMode);
 
   const locale = useLocale();
@@ -14,7 +17,45 @@ const ContactMeForm = () => {
 
   return (
     <div className='relative flex max-w-[500px] min-h-[550px] w-full border p-[20px] border-[#9911ff] rounded-4xl z-20'>
-      <form className='flex flex-col gap-[40px] w-full h-full'>
+      <form
+        className='flex flex-col gap-[40px] w-full h-full relative z-20'
+        onSubmit={(e) => {
+          e.preventDefault();
+
+          setIsSubmitting(true);
+
+          const form = e.target as HTMLFormElement;
+          const fullName = (form[0] as HTMLInputElement).value;
+          const email = (form[1] as HTMLInputElement).value;
+          const message = (form[2] as HTMLTextAreaElement).value;
+
+          const subject = encodeURIComponent('Contact Form Message');
+          const body = encodeURIComponent(
+            `Full Name: ${fullName}\nEmail: ${email}\nMessage: ${message}`
+          );
+
+          const mailtoLink = `mailto:nozadzegiorgi1011@gmail.com?subject=${subject}&body=${body}`;
+          const gmailLink = `https://mail.google.com/mail/?view=cm&fs=1&to=nozadzegiorgi1011@gmail.com&su=${subject}&body=${body}`;
+
+          const isIOS =
+            typeof window !== 'undefined' &&
+            /iPad|iPhone|iPod/.test(navigator.userAgent) &&
+            !(window as any).MSStream;
+
+          if (isIOS) {
+            window.location.href = mailtoLink;
+          } else {
+            const newWindow = window.open(gmailLink, '_blank');
+            if (!newWindow) {
+              alert('Please allow pop-ups to send the message.');
+            }
+          }
+
+          form.reset();
+
+          setTimeout(() => setIsSubmitting(false), 1000);
+        }}
+      >
         <label className='flex flex-col '>
           <LangTranstionSpan
             title='Contact.fullName'
@@ -61,15 +102,22 @@ const ContactMeForm = () => {
         </label>
         <button
           type='submit'
-          className='w-[100px] h-[40px] rounded-4xl bg-[#9911ff] flex justify-center items-center  self-center cursor-pointer'
+          className='min-w-[100px] h-[40px] rounded-4xl bg-[#9911ff] flex justify-center items-center  self-center cursor-pointer'
         >
-          <LangTranstionSpan
-            title='Contact.submit'
-            className='firaCode m-[10px]  text-[white] text-center'
-          />
+          {isSubmitting ? (
+            <LangTranstionSpan
+              title='Contact.Submitting'
+              className='firaCode m-[10px]  text-[white] text-center'
+            />
+          ) : (
+            <LangTranstionSpan
+              title='Contact.submit'
+              className='firaCode m-[10px]  text-[white] text-center'
+            />
+          )}
         </button>
       </form>
-      <div className='absolute bottom-[-200px] left-[-300px] z-10 w-[500px] h-[500px] rounded-full bg-[#340a83] blur-[300px]' />
+      <div className='absolute bottom-[-200px] left-[-300px] z-10 w-[500px] h-[500px] rounded-full bg-[#340a83] blur-[300px] ' />
     </div>
   );
 };
